af53640a0be96eb416383a43a6cd1d44a6f31068
Your Name <you@example.com>
2026-06-17 19:50:04 +0530

    added middelare and logging

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.api import routes_auth, routes_predict
+from app.middleware.logging_middleware import LoggingMiddleware
+
+from app.core.exceptions import register_exception_handlers
+
+app = FastAPI(title="Car price prediction API")
+
+# link middelware
+app.add_middleware(LoggingMiddleware)
+
+# link endpoints
+app.include_router(routes_auth.router, tags=["Auth"])
+
+app.include_router(routes_predict.rounter, tags=["prediction"])
+
+
+# monitoring uisng prometheus
+Instrumentator().instrument(app).expose(app)
+
+
+# add exception
+register_exception_handlers(app)
